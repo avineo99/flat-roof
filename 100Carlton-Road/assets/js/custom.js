dea6b6bf4b3fec9a183c8cb8f7a4f2119aa0197a
avineo99 <avishek.neo99@gmail.com>
2025-04-02 16:08:37 +0530

    add

---
+++ 100Carlton-Road/assets/js/custom.js
@@ -0,0 +1,69 @@
+$(document).on('click', '.toggle', function () {
+  $("body").addClass("menuToggle");
+});
+$(document).on('click', '.close, .menuOverlay', function () {
+  $("body").removeClass("menuToggle");
+});
+$('.testimonial-wrap').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1023,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
+
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#ScrollToTop').fadeIn();
+        } else {
+            $('#ScrollToTop').fadeOut();
+        }
+		
+		if ($(window).width() > 991.98) {
+			 if ($(document).scrollTop() > 50) {
+				$('#Header').addClass('fixed-top');
+				$('#Header').addClass('shrink');
+			}
+			else {
+				$('#Header').removeClass('fixed-top');
+				$('#Header').removeClass('shrink');
+			}
+		}
+    });
+
+    $('#ScrollToTop').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });  
+	
+	 
+	  
+});
